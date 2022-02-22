d2f062e0617d4962f8c8416051bffa449bd576b2
Nishanth Shetty <nishanth.shetty@netbook.ai>
2022-02-22 17:51:59 +0530

    update spawnersvc.proto as spawner.proto

--- proto/compile.sh
+++ proto/compile.sh
@@ -14,5 +14,5 @@
 
 PROTO_DIR="./proto/netbookdevs/spawnerservice"
 
-protoc $PROTO_DIR/spawnersvc.proto --go_out=. --go_opt=paths=source_relative
-protoc $PROTO_DIR/spawnersvc.proto --go-grpc_out=. --go-grpc_opt=paths=source_relative
+protoc $PROTO_DIR/spawner.proto --go_out=. --go_opt=paths=source_relative
+protoc $PROTO_DIR/spawner.proto --go-grpc_out=. --go-grpc_opt=paths=source_relative
