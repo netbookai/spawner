73660021d4d487838360de49c30b0438872c2b79
Nishanth Shetty <nishanth.shetty@netbook.ai>
2022-02-23 20:28:49 +0530

    return the handler return values in logger interceptors

--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -20,6 +20,6 @@ func loggingInterceptor(logger *zap.SugaredLogger) grpc.UnaryServerInterceptor {
 
 		resp, err := handler(ctx, req)
 		logger.Infow("spawnerservice", "method", method, "request", request, "response", resp, "error", err, "took", time.Since(begin))
-		return nil, nil
+		return resp, err
 	}
 }
