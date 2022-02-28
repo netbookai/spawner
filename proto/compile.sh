1477fa466c077e0212c3361e7a2b354092061302
Nishanth Shetty <nishanth.shetty@netbook.ai>
2022-02-28 22:27:34 +0530

    remove unwanted comment and add read and write credentials rpc definition

--- proto/compile.sh
+++ proto/compile.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env sh
-
-# Install proto3 from source
-#  git clone https://github.com/google/protobuf
-#  ./autogen.sh ; ./configure ; make ; make install
-#
-# Install proto
-# Update protoc Go bindings via
-#  go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-#  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-#
-# See also
-#  https://github.com/grpc/grpc-go/tree/master/examples
-
 PROTO_DIR="./proto/netbookdevs/spawnerservice"
 
 protoc $PROTO_DIR/spawner.proto --go_out=. --go_opt=paths=source_relative
