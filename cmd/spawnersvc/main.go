d8e464924aeec402b5e0ca685baec3aebb4b85f0
Nishanth Shetty <nishanth.shetty@netbook.ai>
2022-03-03 14:57:40 +0530

    refactor service startups, add prome request handler to http mux

--- cmd/spawnersvc/main.go
+++ cmd/spawnersvc/main.go
@@ -9,11 +9,9 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/go-kit/kit/metrics"
-	"github.com/go-kit/kit/metrics/prometheus"
 	"github.com/netbook-ai/interceptors"
 	"github.com/oklog/oklog/pkg/group"
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gitlab.com/netbook-devs/spawner-service/pkg/config"
 	"gitlab.com/netbook-devs/spawner-service/pkg/endpoint"
 	"gitlab.com/netbook-devs/spawner-service/pkg/spawnerservice"
@@ -22,6 +20,75 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
+
+func startHttpServer(g *group.Group, config config.Config, logger *zap.SugaredLogger) {
+
+	address := fmt.Sprintf("%s:%d", "", config.DebugPort)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		logger.Errorw("startHttpServer: failed to listen", "error", err)
+		os.Exit(1)
+
+	}
+
+	router := http.NewServeMux()
+
+	router.Handle("/metrics", promhttp.Handler())
+
+	g.Add(func() error {
+
+		logger.Infow("startHttpServer", "transport", "debug/HTTP", "address", address)
+		return http.Serve(listener, router)
+	}, func(err error) {
+		logger.Errorw("http-listener", "error", err)
+		listener.Close()
+	})
+}
+
+func startGRPCServer(g *group.Group, config config.Config, logger *zap.SugaredLogger) {
+
+	address := fmt.Sprintf("%s:%d", "", config.Port)
+	service := spawnerservice.New(logger, &config)
+	endpoints := endpoint.New(service, logger)
+	grpcServer := transport.NewGRPCServer(endpoints, logger)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		logger.Errorw("startGRPCServer", "transport", "gRPC", "during", "Listen", "error", err)
+		os.Exit(1)
+	}
+
+	g.Add(func() error {
+		logger.Infow("startGRPCServer", "transport", "gRPC", "address", address)
+
+		baseServer := grpc.NewServer(interceptors.GetInterceptors("spawnerservice", logger))
+
+		proto.RegisterSpawnerServiceServer(baseServer, grpcServer)
+		return baseServer.Serve(listener)
+	}, func(error) {
+		logger.Errorw("startGRPCServer", "error", err)
+		listener.Close()
+	})
+
+}
+
+func startSignalHandler(g *group.Group) {
+
+	cancelInterrupt := make(chan struct{})
+	g.Add(func() error {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		select {
+		case sig := <-c:
+			return fmt.Errorf("received signal %s", sig)
+		case <-cancelInterrupt:
+			return nil
+		}
+	}, func(error) {
+		close(cancelInterrupt)
+	})
+}
 
 func main() {
 
@@ -45,65 +112,11 @@ func main() {
 	if err != nil {
 		sugar.Errorw("error loading config", "error", err.Error())
 	}
-
-	var duration metrics.Histogram
-	{
-		// Endpoint-level metrics.
-		duration = prometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-			Namespace: "spawnerservice",
-			Subsystem: "spawnerservice",
-			Name:      "request_duration_seconds",
-			Help:      "Request duration in seconds.",
-		}, []string{"method", "success"})
-	}
-
-	service := spawnerservice.New(sugar, &config)
-	endpoints := endpoint.New(service, sugar, duration)
-	grpcServer := transport.NewGRPCServer(endpoints, sugar)
-
 	var g group.Group
-	debugAddr := fmt.Sprintf("%s:%d", "", config.DebugPort)
-	debugListener, err := net.Listen("tcp", debugAddr)
-	if err != nil {
-		sugar.Errorw("error in debugListener", "transport", "debug/HTTP", "during", "Listen", "error", err)
-		os.Exit(1)
-	}
-	g.Add(func() error {
-		sugar.Infow("in debugListener", "transport", "debug/HTTP", "debugAddr", debugAddr)
-		return http.Serve(debugListener, http.DefaultServeMux)
-	}, func(error) {
-		debugListener.Close()
-	})
 
-	grpcAddr := fmt.Sprintf("%s:%d", "", config.Port)
-	grpcListener, err := net.Listen("tcp", grpcAddr)
-	if err != nil {
-		sugar.Errorw("error in grpcListener", "transport", "gRPC", "during", "Listen", "error", err)
-		os.Exit(1)
-	}
+	startHttpServer(&g, config, sugar)
+	startGRPCServer(&g, config, sugar)
+	startSignalHandler(&g)
 
-	g.Add(func() error {
-		sugar.Infow("in main", "transport", "gRPC", "grpcAddr", grpcAddr)
-		baseServer := grpc.NewServer(interceptors.GetInterceptors("spawnerservice", sugar))
-
-		proto.RegisterSpawnerServiceServer(baseServer, grpcServer)
-		return baseServer.Serve(grpcListener)
-	}, func(error) {
-		grpcListener.Close()
-	})
-
-	cancelInterrupt := make(chan struct{})
-	g.Add(func() error {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-		select {
-		case sig := <-c:
-			return fmt.Errorf("received signal %s", sig)
-		case <-cancelInterrupt:
-			return nil
-		}
-	}, func(error) {
-		close(cancelInterrupt)
-	})
 	sugar.Infow("main", "exit", g.Run())
 }
