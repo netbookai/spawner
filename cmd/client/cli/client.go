98c8dcf940adb49265eb26b3a28374fa846d72bd
NishanthSpShetty <nishanthspshetty@gmail.com>
2022-04-12 18:14:52 +0530

    remove zap reference from cli

--- cmd/client/cli/client.go
+++ cmd/client/cli/client.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
@@ -16,23 +15,17 @@ var rootCommand = &cobra.Command{
 }
 
 func getSpawnerConn(addr string) (*grpc.ClientConn, error) {
-
 	log.Println("connecting to ", addr)
 	return grpc.Dial(addr, grpc.WithInsecure(), grpc.WithTimeout(time.Second))
 }
 
-func setupCommands(l *zap.SugaredLogger) {
+func setupCommands() {
 	rootCommand.AddCommand(createCluster())
 	rootCommand.AddCommand(clusteStatus())
 	rootCommand.AddCommand(deleteCluster())
 }
 
 func Execute() error {
-	z, err := zap.NewDevelopment()
-	if err != nil {
-		return err
-	}
-	logger := z.Sugar()
-	setupCommands(logger)
+	setupCommands()
 	return rootCommand.Execute()
 }
