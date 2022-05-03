7d70ff78a31adcb4e4d443c50df06a233780ee4e
NishanthSpShetty <nishanthspshetty@gmail.com>
2022-05-03 14:58:13 +0530

    move duplicate machine types to constant lists

--- pkg/service/common/machine.go
+++ pkg/service/common/machine.go
@@ -4,6 +4,20 @@ import (
 	"gitlab.com/netbook-devs/spawner-service/pkg/service/constants"
 )
 
+//machine type constants
+
+const S = "s"
+const M = "m"
+const L = "l"
+const XL = "xl"
+const MT4 = "m+t4"
+const Mk80 = "m+k80"
+const Lk80 = "l+k80"
+const XLk80 = "xl+k80"
+const Mv100 = "m+v100"
+const Lv100 = "l+v100"
+const XLv100 = "xl+v100"
+
 type InstanceSizeMap map[string]string
 
 var providerInstanceType map[string]InstanceSizeMap
@@ -11,34 +25,34 @@ var providerInstanceType map[string]InstanceSizeMap
 func azure() InstanceSizeMap {
 	m := InstanceSizeMap{
 
-		"s":       "Standard_B1s",
-		"m":       "Standard_F8s_v2",
-		"l":       "Standard_F32s_v2",
-		"xl":      "Standard_F64_v2",
-		"m+t4":    "Standard_NC4as_T4_v3",
-		"m+k80":   "Standard_NC6",
-		"l+k80":   "Standard_NC12",
-		"xl+k80":  "Standard_NC24",
-		"m+v100":  "Standard_NC6s_v3",
-		"l+v100":  "Standard_NC12s_v3",
-		"xl+v100": "Standard_NC24s_v3",
+		S:      "Standard_B1s",
+		M:      "Standard_F8s_v2",
+		L:      "Standard_F32s_v2",
+		XL:     "Standard_F64_v2",
+		MT4:    "Standard_NC4as_T4_v3",
+		Mk80:   "Standard_NC6",
+		Lk80:   "Standard_NC12",
+		XLk80:  "Standard_NC24",
+		Mv100:  "Standard_NC6s_v3",
+		Lv100:  "Standard_NC12s_v3",
+		XLv100: "Standard_NC24s_v3",
 	}
 	return m
 }
 
 func aws() InstanceSizeMap {
 	m := InstanceSizeMap{
-		"s":       "t2.micro",
-		"m":       "m5.2xlarge",
-		"l":       "m5.8xlarge",
-		"xl":      "m5.16xlarge",
-		"m+t4":    "g4dn.xlarge",
-		"m+k80":   "p2.xlarge",
-		"l+k80":   "p2.8xlarge",
-		"xl+k80":  "p2.16xlarge",
-		"m+v100":  "p3.xlarge",
-		"l+v100":  "p3.8xlarge",
-		"xl+v100": "p3.16xlarge",
+		S:      "t2.micro",
+		M:      "m5.2xlarge",
+		L:      "m5.8xlarge",
+		XL:     "m5.16xlarge",
+		MT4:    "g4dn.xlarge",
+		Mk80:   "p2.xlarge",
+		Lk80:   "p2.8xlarge",
+		XLk80:  "p2.16xlarge",
+		Mv100:  "p3.xlarge",
+		Lv100:  "p3.8xlarge",
+		XLv100: "p3.16xlarge",
 	}
 	return m
 }
@@ -46,17 +60,17 @@ func aws() InstanceSizeMap {
 func gcp() InstanceSizeMap {
 	// https://github.com/iterative/terraform-provider-iterative/blob/master/iterative/gcp/provider.go#L415
 	m := InstanceSizeMap{
-		"s":       "g1-small",
-		"m":       "e2-custom-8-32768",
-		"l":       "e2-custom-32-131072",
-		"xl":      "n2-custom-64-262144",
-		"m+t4":    "n1-standard-4",
-		"m+k80":   "custom-8-53248",
-		"l+k80":   "custom-32-131072",
-		"xl+k80":  "custom-64-212992-ext",
-		"m+v100":  "custom-8-65536-ext",
-		"l+v100":  "custom-32-262144-ext",
-		"xl+v100": "custom-64-524288-ext",
+		S:      "g1-small",
+		M:      "e2-custom-8-32768",
+		L:      "e2-custom-32-131072",
+		XL:     "n2-custom-64-262144",
+		MT4:    "n1-standard-4",
+		Mk80:   "custom-8-53248",
+		Lk80:   "custom-32-131072",
+		XLk80:  "custom-64-212992-ext",
+		Mv100:  "custom-8-65536-ext",
+		Lv100:  "custom-32-262144-ext",
+		XLv100: "custom-64-524288-ext",
 	}
 	return m
 }
