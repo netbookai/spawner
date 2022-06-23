6d162eeec0f5586b10270c39bdc66469501783b8
NishanthSpShetty <nishanthspshetty@gmail.com>
2022-06-23 22:40:36 +0530

    fixes#65 missing region when creation session with user home cred

--- pkg/service/system/secrets.go
+++ pkg/service/system/secrets.go
@@ -83,10 +83,11 @@ func getSessionUsingAwsAccessKey(region string) (*session.Session, error) {
 	return sess, err
 }
 
-func getSessionUsingAwsProfile(profile string) (*session.Session, error) {
+func getSessionUsingAwsProfile(profile, region string) (*session.Session, error) {
 
 	sess, err := session.NewSessionWithOptions(
 		session.Options{
+			Config:  aws.Config{Region: &region},
 			Profile: profile,
 		},
 	)
@@ -112,11 +113,10 @@ func getLocalEnvSession(region string) (*session.Session, error) {
 	if config.Get().AWSAccessID != "" {
 		// creating creds from config variables
 		return getSessionUsingAwsAccessKey(region)
-
 	}
 
 	log.Println("AWSAccessID is empty, using 'default' profile from user's home directory")
-	return getSessionUsingAwsProfile("default")
+	return getSessionUsingAwsProfile("default", region)
 
 }
 
